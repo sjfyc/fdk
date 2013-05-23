a1a65aaf9519b528ddc95e05103c08f52b3e6568
sunjin <sjfyc@sina.com>
2013-05-23 15:19:44 +0800

    debughook complete

--- tutorials/tutorlua/debughook/main.c
+++ tutorials/tutorlua/debughook/main.c
@@ -3,12 +3,12 @@
 #include <lualib.h>
 #include <stdio.h>
 
-int func_c(lua_State *L);
+int func(lua_State *L);
 
 int main(int argc, char *argv[])
 {
-	lua_State *L;  
-
+	lua_State *L;
+	
 	if ((L = lua_open()) == NULL) 
 	{
 		printf("lua_open() failed!\n");
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	}
 	luaL_openlibs(L);
 
-	lua_register(L, "func_c", func_c);
+	lua_register(L, "func", func);
 
 	if (luaL_dofile(L, "main.lua")) 
 	{
@@ -30,8 +30,17 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int func_c(lua_State *L)
+int func(lua_State *L)
 {
-
-	return 2;
+	lua_Debug ar;
+	int i;
+	for (i = 0; i < 3; ++i)
+	{
+		if (lua_getstack(L, i, &ar)) 
+		{
+			lua_getinfo(L, "nSl", &ar);
+			printf("[%d]%s %s:%d\n", i, ar.name, ar.short_src, ar.currentline);
+		}
+	}
+	return 0;
 }
--- tutorials/tutorlua/debughook/main.lua
+++ tutorials/tutorlua/debughook/main.lua
@@ -1,5 +1,15 @@
-print("func_c(2, 3)=",func_c(2, 3))
-
-function func_lua(a, b)
-  return a+b, a-b
+function test()
+	test2()
 end
+
+function test2()
+	func()
+end
+
+test()
+
+--[[ Desc the stack
+[0]func,?:? ==> 现在在func函数内，但运行在哪个文件哪行是未知的
+[1]test2,main.lua:6
+[2]test,main.lua:2
+--]]
