f8f5b278971f1b20d36f56062817abe810793e03
sunjin <sjfyc@sina.com>
2013-06-02 15:50:31 +0800

    xxx

--- tutorials/tutorlua/debughook/main.lua
+++ tutorials/tutorlua/debughook/main.lua
@@ -9,7 +9,7 @@ end
 -- test() call from c++
 
 --[[ Desc the stack
-[0]func [C]:-1 ==> 现在在func函数内，由于是C函数所以不知道具体在哪个文件的哪行
+[0]func [C]:-1 ==> 现在C的func函数内，由于是C函数所以不知道具体在哪个文件的哪行
 [1]test2 main.lua:6
 [2](null) main.lua:2 ==> 从c++发起的lua函数调用名称是未知的
 --]]
