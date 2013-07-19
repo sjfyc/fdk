27fed001a0884c0ae504e25ea64768a4b621ba02
sunjin <sjfyc@sina.com>
2013-07-19 21:59:34 +0800

    initRand,rand01

--- fdk/include/fdk/Rand.h
+++ fdk/include/fdk/Rand.h
@@ -1,17 +1,37 @@
 #ifndef __FDK_RAND_H_INCLUDE__
 #define __FDK_RAND_H_INCLUDE__
-#include <stdlib.h>
 #include "Base.h"
+#include <stdlib.h>
+#include <time.h>
 
 namespace fdk
 {
 	// 需要先初始化随机数种子：srand(time(0));
-	template <class T>
-	inline T rand(const T& a, const T& b)
+	//template <class T>
+	//inline T rand(const T& a, const T& b)
+	//{
+	//	const T& minVal = minOf(a, b);
+	//	const T& maxVal = maxOf(a, b);
+	//	return static_cast<T>( ((double)::rand()/RAND_MAX)*(maxVal-minVal) + minVal );
+	//}
+
+	void initRand()
 	{
-		const T& minVal = minOf(a, b);
-		const T& maxVal = maxOf(a, b);
-		return static_cast<T>( ((double)::rand()/RAND_MAX)*(maxVal-minVal) + minVal );
+		::srand((unsigned int)(time(0)));
+	}
+
+	// [a,b]
+	int rand(int a, int b)
+	{
+		int minValue = a < b ? a : b;
+		int maxValue = a > b ? a : b;
+		return (::rand() % (maxValue-minValue+1))+ minValue;
+	}
+
+	// [0,1]
+	double rand01()
+	{
+		return ::rand() / double(RAND_MAX);
 	}
 }
 
